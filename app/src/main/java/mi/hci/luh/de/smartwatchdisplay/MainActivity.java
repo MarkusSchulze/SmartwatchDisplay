ce61c60f4f278216cdd2cf30b1470d58f80eebe4
MarkusSchulze <markus-schulze@gmx.net>
2016-12-22 11:33:56 +0100

    Merge branch 'master' of D:\Prog\SmartwatchDisplay with conflicts.

--- app/src/main/java/mi/hci/luh/de/smartwatchdisplay/MainActivity.java
+++ app/src/main/java/mi/hci/luh/de/smartwatchdisplay/MainActivity.java
@@ -1,80 +1,113 @@
 package mi.hci.luh.de.smartwatchdisplay;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  implements SensorEventListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.Wearable;
 
-    private SensorManager mSensorManager;
-    private CursorView cursorView;
-    private LinearLayout layout;
-    private Sensor accel;
+import java.util.ArrayList;
+import java.util.Set;
+
+import static android.R.id.list;
+import static android.content.ContentValues.TAG;
+import static com.google.android.gms.wearable.CapabilityApi.FILTER_REACHABLE;
+import static mi.hci.luh.de.smartwatchdisplay.BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED;
+
+public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int REQUEST_ENABLE_BT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // Get an instance of the SensorManager
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        TextView txt_output = (TextView) findViewById(R.id.output);
 
-        // -----------------------------------------------
-        // Drawing with Android Motion Sensors
-        // dynamisch mit accel Sensorvalues
-        cursorView = new CursorView(this);
-        setContentView(cursorView);
-        accel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        BluetoothManager btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+
+        BluetoothAdapter btAdapter = btManager.getAdapter();
+        if (btAdapter != null && !btAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
+        }
+
+//        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+//        BluetoothGatt mBluetoothGatt;
+//
+//        if (pairedDevices.size() > 0) {
+//            for (BluetoothDevice bt : pairedDevices) {
+//                txt_output.setText(bt.getName() + " - " + bt.getAddress());
+//                mBluetoothGatt = bt.connectGatt(this, false, btleGattCallback);
+//                mBluetoothGatt.discoverServices();
+//            }
+//        } else {
+//            Log.d("msg","No Paired Bluetooth Devices Found.");
+//        }
+
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addApi(Wearable.API)
+                .build();
+
+        PendingResult result;
+        result = Wearable.CapabilityApi.getAllCapabilities(mGoogleApiClient, FILTER_REACHABLE);
+
+        //Wearable.ChannelApi.openChannel(mGoogleApiClient,)
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // don't do anything; we don't care
-    }
-    public void onSensorChanged(SensorEvent event) {
-        final float alpha = (float) 0.8;
 
-        // Isolate the force of gravity with the low-pass filter.
-        float[] gravity = new float[3];
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
 
-        // Remove the gravity contribution with the high-pass filter.
-        float[] linear_acceleration = new float[3];
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];
+    private final BluetoothGattCallback btleGattCallback = new BluetoothGattCallback() {
 
-        //float Gx = -event.values[0];
-        //float Gy = event.values[1];
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+            // this will get called anytime you perform a read or write characteristic operation
+        }
 
-        float Gx = linear_acceleration[0];
-        float Gy = linear_acceleration[1];
+        @Override
+        public void onConnectionStateChange(final BluetoothGatt gatt, final int status, final int newState) {
+            // this will get called when a device connects or disconnects
+        }
 
-        cursorView.move(Gx, Gy);
-        cursorView.invalidate();
+        @Override
+        public void onServicesDiscovered(final BluetoothGatt gatt, final int status) {
+            // this will get called after the client initiates a 			BluetoothGatt.discoverServices() call
+        }
+    };
 
-        //TODO: Calculate Distance and use it for move the Cursor
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        if (status == BluetoothGatt.GATT_SUCCESS) {
+            Log.w(TAG, "onServicesDiscovered received: " + status);
+        } else {
+            Log.w(TAG, "onServicesDiscovered received: " + status);
+        }
     }
 
     @Override
-    protected void onResume() {
-        // Ideally a game should implement onResume() and onPause()
-        // to take appropriate action when the activity looses focus
-        super.onResume();
-        mSensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        if (result.getErrorCode() == ConnectionResult.API_UNAVAILABLE) {
+            // The Wearable API is unavailable
+            Log.d("Error","Smartwatch not available");
+        }
     }
-
-    @Override
-    protected void onPause() {
-        // Ideally a game should implement onResume() and onPause()
-        // to take appropriate action when the activity looses focus
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
 }
+
+
